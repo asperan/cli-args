5497a015b36c52ea1a9d5b465da393bc922ce20a
Alex Speranza <alex.speranza@studio.unibo.it>
2021-10-14 10:31:31 +0200

    Improve documentation comments

--- source/asperan/cli_args/option_parser.d
+++ source/asperan/cli_args/option_parser.d
@@ -1,11 +1,17 @@
 module asperan.cli_args.option_parser;
 
 /**
- * Option object. It has a short name (for example "-h"), a long name (for example "--help") and a description.
+ * Option object.
+ *
+ * It has a short name (for example "-h"), a long name (for example "--help") and a description.
+ *
  * Usually on UNIX systems the short version of an option starts with "-" (one hyphen) and the long version starts with "--" (two hyphens).
+ *
  * Also, short names and long names should be unique among all options (i.e. there cannot be two options with the same short name or the same long name).
+ *
  * This type of options has a side effect which does not need an argument (flag option, like --verbose) or 
- * a side effect which accepts a string value as argument (the passed value can then be parsed to the desired type, for example "int").
+ * a side effect which accepts a string value as argument (the passed value can then be parsed to the desired type, for example "int"), 
+ * which usually is the next CLI argument to be parsed.
  */
 class CommandLineOption {
   /// The short name of the option.
@@ -42,10 +48,10 @@ class CommandLineOption {
   /// Returns whether the option needs an additional argument
   bool needsArgument() { return stringSideEffect.funcptr != null; }
 
-  /// Executes the side effect with no arguments without checks.
+  /// Executes the side effect with no arguments without checks on the actual presence of the function.
   void runVoidSideEffect() { this.voidSideEffect(); }
 
-  /// Executes the side effect with a string arguments without checks.
+  /// Executes the side effect with a string arguments without checks on the actual presence of the function.
   void runStringSideEffect(in string value) { this.stringSideEffect(value); }
 }
 
