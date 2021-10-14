b42982525742c132706a36faf86032642505d60e
Alex Speranza <alex.speranza@studio.unibo.it>
2021-10-14 10:32:21 +0200

    Move private class to the bottom of the file, so exposed declarations are at the top

--- source/asperan/cli_args/simple_option_parser.d
+++ source/asperan/cli_args/simple_option_parser.d
@@ -3,41 +3,6 @@ module asperan.cli_args.simple_option_parser;
 public import asperan.cli_args.option_parser;
 import asperan.option;
 
-private class SimpleOptionParser : CommandLineOptionParser {
-  private CommandLineOption[] options;
-
-  CommandLineOption[] getOptions() const { return cast(CommandLineOption[])options[]; }
-
-  string[] parse(in string[] arguments) {
-		string[] output;
-		for(size_t index = 0; index < arguments.length; index++) {
-      string arg = arguments[index];
-      Option!CommandLineOption correspondingOption = findOption(arg);
-      if (!correspondingOption.isEmpty) {
-        if (correspondingOption.get.needsArgument) {
-          if (index + 1 >= arguments.length) { throw new Error("Last option needed an argument but no more arguments were given."); }
-					else {
-						correspondingOption.get.runStringSideEffect(arguments[index + 1]);
-						index += 1;
-					}
-        } else { correspondingOption.get.runVoidSideEffect(); }
-      } else { output ~= arg; }
-    }
-    return output;
-  }
-
-  private Option!CommandLineOption findOption(in string value) {
-		foreach(size_t index, CommandLineOption o; this.options) {
-      if (o.shortName == value || o.longName == value) {
-        return Option!CommandLineOption.some(this.options[index]);
-      }
-    }
-    return Option!CommandLineOption.none();
-  }
-
-  private void registerOption(CommandLineOption opt) { options ~= opt; }
-}
-
 /**
  * A builder for an OptionParser which insert as command line argument all the unrecognized options;
  * the only error thrown is when the last string value is a recognized option which needs a secondary argument.
@@ -88,3 +53,48 @@ string getSimpleHelpMessage(in CommandLineOptionParser parser) {
   return options.map!(o => leftJustify(o.shortName, maxShortVersionLength) ~ " " ~ leftJustify(o.longName, maxLongVersionLength) ~ " " ~ o.description ~ "\n")
                 .reduce!"a ~ b";
 }
+
+/**
+ * Error thrown when the last CLI option requires an additional value (but it is not provided as the option is the last argument).
+ */
+final class NoArgumentForLastOptionError : Error {
+  this() {
+    super("Last option needed an argument but no more arguments were given.");
+  }
+}
+
+private class SimpleOptionParser : CommandLineOptionParser {
+  private CommandLineOption[] options;
+
+  CommandLineOption[] getOptions() const { return cast(CommandLineOption[])options[]; }
+
+  string[] parse(in string[] arguments) {
+		string[] output;
+		for(size_t index = 0; index < arguments.length; index++) {
+      string arg = arguments[index];
+      Option!CommandLineOption correspondingOption = findOption(arg);
+      if (!correspondingOption.isEmpty) {
+        if (correspondingOption.get.needsArgument) {
+          if (index + 1 >= arguments.length) { throw new NoArgumentForLastOptionError(); }
+					else {
+						correspondingOption.get.runStringSideEffect(arguments[index + 1]);
+						index += 1;
+					}
+        } else { correspondingOption.get.runVoidSideEffect(); }
+      } else { output ~= arg; }
+    }
+    return output;
+  }
+
+  private Option!CommandLineOption findOption(in string value) {
+		foreach(size_t index, CommandLineOption o; this.options) {
+      if (o.shortName == value || o.longName == value) {
+        return Option!CommandLineOption.some(this.options[index]);
+      }
+    }
+    return Option!CommandLineOption.none();
+  }
+
+  private void registerOption(CommandLineOption opt) { options ~= opt; }
+}
+
