df14cc20bf34233a80818709fd95f77b4b248dab
Alex Speranza <alex.speranza@studio.unibo.it>
2022-01-29 10:47:51 +0100

    refactor: fix indentation and documentation style

--- source/asperan/cli_args/simple_option_parser.d
+++ source/asperan/cli_args/simple_option_parser.d
@@ -3,95 +3,134 @@ module asperan.cli_args.simple_option_parser;
 public import asperan.cli_args.option_parser;
 
 /**
- * A builder for an OptionParser which insert as command line argument all the unrecognized options;
- * the only error thrown is when the last string value is a recognized option which needs a secondary argument.
+ *  A builder for an OptionParser which insert as command line argument all the unrecognized options;
+ *  the only error thrown is when the last string value is a recognized option which needs a secondary argument.
  *
- * Examples:
- * --------------------------
- * CommandLineOptionParser op = new SimpleOptionParserBuilder()
- *   .addOption("-v", "--version", "Print the program version and exit", () {writeln("version");})
- *   .addOption("-h", "--help", "Print the help message and exit", () {writeln("help");})
- *   .addOption("-d", "--debug", "Print the next argument", (string arg) {writeln(arg);})
- *   .build(); 
- * --------------------------
+ *  Examples:
+ *  --------------------------
+ *  CommandLineOptionParser op = new SimpleOptionParserBuilder()
+ *      .addOption("-v", "--version", "Print the program version and exit", () {writeln("version");})
+ *      .addOption("-h", "--help", "Print the help message and exit", () {writeln("help");})
+ *      .addOption("-d", "--debug", "Print the next argument", (string arg) {writeln(arg);})
+ *      .build(); 
+ *  --------------------------
  */
-class SimpleOptionParserBuilder {
+class SimpleOptionParserBuilder
+{
 
-  private SimpleOptionParser parser; 
+    private SimpleOptionParser parser; 
 
-  this() { this.parser = new SimpleOptionParser(); }
+    /**
+     * Ctor.
+     */
+    this() { this.parser = new SimpleOptionParser(); }
 
-  /**
-   * Returns the configured parser.
-   */
-  CommandLineOptionParser build() { return this.parser; }
+    /**
+     *  Returns the configured parser.
+     */
+    CommandLineOptionParser build() { return this.parser; }
 
-  /**
-   * Adds an option to the parser. The option has a side effect which does not require an argument.
-   * This method returns the builder it is called on, so it can be chained with other calls.
-   */
-  SimpleOptionParserBuilder addOption(in string shortName, in string longName, in string description, in void delegate() voidSideEffect) {
-    this.parser.registerOption(new CommandLineOption(shortName, longName, description, voidSideEffect));
-    return this;
-	}
+    /**
+     *  Adds an option to the parser. The option has a side effect which does not require an argument.
+     *  This method returns the builder it is called on, so it can be chained with other calls.
+     */
+    SimpleOptionParserBuilder addOption(
+        in string shortName,
+        in string longName,
+        in string description,
+        in void delegate() voidSideEffect
+    )
+    {
+        this.parser.registerOption(new CommandLineOption(shortName, longName, description, voidSideEffect));
+        return this;
+    }
 
-  /**
-   * Adds an option to the parser. The option has a side effect which requires an argument.
-   * This method returns the builder it is called on, so it can be chained with other calls.
-   */
-	SimpleOptionParserBuilder addOption(in string shortName, in string longName, in string description, in void delegate(string) stringSideEffect) {
-    this.parser.registerOption(new CommandLineOption(shortName, longName, description, stringSideEffect));
-    return this;
-	}
+    /**
+     *  Adds an option to the parser. The option has a side effect which requires an argument.
+     *  This method returns the builder it is called on, so it can be chained with other calls.
+     */
+    SimpleOptionParserBuilder addOption(
+        in string shortName,
+        in string longName,
+        in string description,
+        in void delegate(string) stringSideEffect
+    )
+    {
+        this.parser.registerOption(new CommandLineOption(shortName, longName, description, stringSideEffect));
+        return this;
+    }
 }
 
 /**
- * Returns a formatted string with the short name, the long name and the description of the options of the given parser.
+ *  Returns a formatted string with the short name, the long name and the description of the options of the given parser.
  */
-string getSimpleHelpMessage(in CommandLineOptionParser parser) {
-  import std.algorithm.iteration : map, reduce; import std.algorithm.searching : maxElement; import std.string : leftJustify;
-  CommandLineOption[] options = parser.getOptions;
-  size_t maxShortVersionLength = options.map!(o => o.shortName.length).maxElement;
-  size_t maxLongVersionLength = options.map!(o => o.longName.length).maxElement;
-  return options.map!(o => leftJustify(o.shortName, maxShortVersionLength) ~ " " ~ leftJustify(o.longName, maxLongVersionLength) ~ " " ~ o.description ~ "\n")
-                .reduce!"a ~ b";
+string getSimpleHelpMessage(in CommandLineOptionParser parser)
+{
+    import std.algorithm.iteration : map, reduce;
+    import std.algorithm.searching : maxElement;
+    import std.string : leftJustify;
+
+    CommandLineOption[] options = parser.getOptions;
+    size_t maxShortVersionLength = options.map!(o => o.shortName.length).maxElement;
+    size_t maxLongVersionLength = options.map!(o => o.longName.length).maxElement;
+    return options
+            .map!(
+                o => 
+                    leftJustify(o.shortName, maxShortVersionLength) ~ 
+                    " " ~
+                    leftJustify(o.longName, maxLongVersionLength) ~
+                    " " ~
+                    o.description ~
+                    "\n"
+            )
+            .reduce!"a ~ b";
 }
 
-private class SimpleOptionParser : CommandLineOptionParser {
-  import std.typecons : Nullable;
+private class SimpleOptionParser : CommandLineOptionParser
+{
+    import std.typecons : Nullable;
 
-  private CommandLineOption[] options;
+    private CommandLineOption[] options;
 
-  CommandLineOption[] getOptions() const { return cast(CommandLineOption[])options[]; }
+    CommandLineOption[] getOptions() const { return cast(CommandLineOption[])options[]; }
 
-  string[] parse(in string[] arguments) {
-    import asperan.cli_args.exception : NoArgumentForLastOptionError;
+    string[] parse(in string[] arguments)
+    {
+        import asperan.cli_args.exception : NoArgumentForLastOptionError;
 		string[] output;
-		for(size_t index = 0; index < arguments.length; index++) {
-      string arg = arguments[index];
-      Nullable!CommandLineOption correspondingOption = findOption(arg);
-      if (!correspondingOption.isNull) {
-        if (correspondingOption.get.needsArgument) {
-          if (index + 1 >= arguments.length) { throw new NoArgumentForLastOptionError(); }
-					else {
+		for(size_t index = 0; index < arguments.length; index++)
+        {
+            string arg = arguments[index];
+            Nullable!CommandLineOption correspondingOption = findOption(arg);
+            if (!correspondingOption.isNull)
+            {
+                if (correspondingOption.get.needsArgument)
+                {
+                    if (index + 1 >= arguments.length) { throw new NoArgumentForLastOptionError(); }
+                    else
+                    {
 						correspondingOption.get.runStringSideEffect(arguments[index + 1]);
 						index += 1;
 					}
-        } else { correspondingOption.get.runVoidSideEffect(); }
-      } else { output ~= arg; }
+                } 
+                else { correspondingOption.get.runVoidSideEffect(); }
+            } 
+            else { output ~= arg; }
+        }
+        return output;
     }
-    return output;
-  }
 
-  void registerOption(CommandLineOption opt) { options ~= opt; }
+    void registerOption(CommandLineOption opt) { options ~= opt; }
 
-  private Nullable!CommandLineOption findOption(in string value) {
-		foreach(size_t index, CommandLineOption o; this.options) {
-      if (o.shortName == value || o.longName == value) {
-        return Nullable!CommandLineOption(this.options[index]);
-      }
+    private Nullable!CommandLineOption findOption(in string value) {
+		foreach(size_t index, CommandLineOption o; this.options)
+        {
+            if (o.shortName == value || o.longName == value)
+            {
+                return Nullable!CommandLineOption(this.options[index]);
+            }
+        }
+        return Nullable!CommandLineOption();
     }
-    return Nullable!CommandLineOption();
-  }
 }
 
