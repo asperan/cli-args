bece4fae9ede1a4c1a6a604ccd03a78cde2b1402
Alex Speranza <alex.speranza@studio.unibo.it>
2022-01-29 10:53:06 +0100

    refactor: fix indentation and documentation style

--- source/asperan/cli_args/exception.d
+++ source/asperan/cli_args/exception.d
@@ -1,11 +1,16 @@
 module asperan.cli_args.exception;
 
 /**
- * Error thrown when the last CLI option requires an additional value (but it is not provided as the option is the last argument).
+ *  Error thrown when the last CLI option requires an additional value (but it is not provided as the option is the last argument).
  */
-final class NoArgumentForLastOptionError : Error {
-  this() {
-    super("Last option needed an argument but no more arguments were given.");
-  }
+final class NoArgumentForLastOptionError : Error
+{
+    /**
+     * Ctor.
+     */
+    this()
+    {
+        super("Last option needed an argument but no more arguments were given.");
+    }
 }
 
